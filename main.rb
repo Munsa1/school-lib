97e0362dbe63ccb39d85a5cdf74f8df931cb33dd
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-02-04 11:46:28 +0200

    Add app class to main.rb

--- main.rb
+++ main.rb
@@ -4,3 +4,11 @@ require_relative 'teacher'
 require_relative 'rental'
 require_relative 'book'
 require_relative 'classroom'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+    @class = Classroom.new('Grade 5')
+  end
