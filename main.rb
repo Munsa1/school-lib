a30e403ab6d7966dd19a4aaf2b91786958acd218
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-02-04 14:14:04 +0200

    Add create_book  method

--- main.rb
+++ main.rb
@@ -118,3 +118,19 @@ class App
     sleep 0.75
     menu
   end
+
+  def create_a_book
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books << book
+
+    puts 'Book added successfully'
+    sleep 0.75
+    menu
+  end
+end
