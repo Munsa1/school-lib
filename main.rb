0aad0b4acb36adc83fb104cabebf0a3a3000273e
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-02-04 14:13:30 +0200

    Add create_a_teacher  method

--- main.rb
+++ main.rb
@@ -100,3 +100,21 @@ class App
     sleep 0.75
     menu
   end
+
+  def create_a_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Specialization: '
+    specialization = gets.chomp
+
+    teacher = Teacher.new(specialization, age, name)
+    @people << teacher
+
+    puts 'Teacher created successfully'
+    sleep 0.75
+    menu
+  end
