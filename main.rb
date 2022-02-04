5ae6ddb1588bbefb749430155c507ce94c92b1a1
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-02-04 14:12:52 +0200

    Add create_a_student  method

--- main.rb
+++ main.rb
@@ -82,3 +82,21 @@ class App
       puts 'Invalid input. Kindly type 1 or 2'
     end
   end
+
+  def create_a_student
+    print 'Age: '
+    age = gets.chomp.to_i
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase
+
+    student = Student.new(@class, age, name, parent_permission)
+    @people << student
+
+    puts 'Student created successfully'
+    sleep 0.75
+    menu
+  end
